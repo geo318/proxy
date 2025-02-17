b220e29ecc6777c702887089b6d297fde7e891b7
geo318 <geo.lomidze@gmail.com>
2025-02-18 01:12:17 +0400

    chore: project scaffolding

---
+++ types/index.ts
@@ -0,0 +1,33 @@
+export interface Product {
+  _id: string
+  title: string
+  cost: number
+  availableQuantity: number
+  isArchived: boolean
+  createdAt?: string
+  updatedAt?: string
+}
+
+export interface CartItem {
+  _id: string
+  cartId: string
+  product: Product
+  quantity: number
+  addedAt: string
+  updatedAt: string
+}
+
+export interface Cart {
+  _id: string
+  hash: string
+  items: CartItem[]
+  createdAt: string
+  updatedAt: string
+}
+
+export type CartItemEvent = 'ITEM_QUANTITY_UPDATED' | 'ITEM_OUT_OF_STOCK'
+
+export interface CartItemMessage {
+  event: CartItemEvent
+  payload: CartItem
+}
