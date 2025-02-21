bdce8685b1c8e5c8a6ff4602441602099011af64
geo318 <geo.lomidze@gmail.com>
2025-02-21 10:46:49 +0400

    style: notifications modal with shadcn dialog

--- components/notification-modal.tsx
+++ components/notification-modal.tsx
@@ -1,32 +1,55 @@
 import { CartItemMessage } from '@/types'
+import {
+  Dialog,
+  DialogFooter,
+  DialogHeader,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from './ui/dialog'
+
+import { Button } from './ui/button'
 
 interface NotificationModalProps {
-  changes: CartItemMessage[]
+  isOpen: boolean
+  changes: CartItemMessage[] | null
   onAcknowledge: () => void
 }
 
 const NotificationModal = ({
+  isOpen,
   changes,
   onAcknowledge,
 }: NotificationModalProps) => {
   return (
-    <div className='fixed inset-0 top-0 bg-gray-100 p-4 border border-gray-300 z-50'>
-      <h2>Cart Updates</h2>
-      <ul>
-        {changes?.map((change) => (
-          <li key={change.payload._id}>
-            <strong>
-              {change.event === 'ITEM_OUT_OF_STOCK'
-                ? 'Out of Stock'
-                : 'Quantity Updated'}
-              :
-            </strong>{' '}
-            {change.payload.product.title}
-          </li>
-        ))}
-      </ul>
-      <button onClick={onAcknowledge}>OK</button>
-    </div>
+    <Dialog open={isOpen} modal={true}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Cart Updates</DialogTitle>
+          <DialogDescription>
+            The following changes occurred to your cart:
+          </DialogDescription>
+        </DialogHeader>
+
+        <ul className='list-disc ml-5 space-y-1'>
+          {changes?.map((change) => (
+            <li key={change.payload._id}>
+              <strong>
+                {change.event === 'ITEM_OUT_OF_STOCK'
+                  ? 'Out of Stock'
+                  : 'Quantity Updated'}
+                :
+              </strong>{' '}
+              {change.payload.product.title}
+            </li>
+          ))}
+        </ul>
+
+        <DialogFooter>
+          <Button onClick={onAcknowledge}>OK</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
 
