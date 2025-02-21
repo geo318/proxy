120701e9c8ffe60ec06bb12b44a948d007a7560f
geo318 <geo.lomidze@gmail.com>
2025-02-21 10:54:40 +0400

    style: design cart item and product card using shadcn

--- components/cart-item.tsx
+++ components/cart-item.tsx
@@ -1,30 +1,81 @@
 import { CartItem as CartItemType } from '@/types'
-import { useCart } from '@/context/global.context'
+import { useGlobalContext } from '@/context/global.context'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card'
+import { Button } from './ui/button'
+import { Label } from './ui/label'
+import { Minus, Plus } from 'lucide-react'
 
 interface CartItemProps {
   item: CartItemType
 }
 
 const CartItem = ({ item }: CartItemProps) => {
-  const { removeItemFromCart, updateItemQuantity } = useCart()
+  const { removeItemFromCart, updateItemQuantity } = useGlobalContext()
 
   return (
-    <li className='mb-4'>
-      <h3>{item.product.title}</h3>
-      <p>Quantity: {item.quantity}</p>
-      <p>Cost: ${item.product.cost}</p>
-      <div>
-        <button onClick={() => updateItemQuantity(item._id, item.quantity + 1)}>
-          +
-        </button>
-        <button
-          onClick={() => updateItemQuantity(item._id, item.quantity - 1)}
-          disabled={item.quantity <= 1}
-        >
-          -
-        </button>
-        <button onClick={() => removeItemFromCart(item._id)}>Remove</button>
-      </div>
+    <li>
+      <Card className='w-[350px]'>
+        <CardHeader>
+          <CardTitle>{item.product.title}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className='grid w-full items-center gap-4'>
+            <div className='flex space-x-1.5'>
+              <Label
+                htmlFor='framework'
+                className='font-bold flex items-center justify-center'
+              >
+                Price:
+              </Label>
+              <p className='font-medium'>${item.product.cost}</p>
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className='flex justify-between'>
+          <section className='flex items-center justify-center self-stretch'>
+            <Button
+              variant={
+                item.quantity >= item.product.availableQuantity
+                  ? 'ghost'
+                  : 'outline'
+              }
+              onClick={() => updateItemQuantity(item._id, item.quantity + 1)}
+              disabled={item.quantity >= item.product.availableQuantity}
+              title={
+                item.quantity >= item.product.availableQuantity
+                  ? 'Cannot add more than available quantity'
+                  : 'Add more'
+              }
+            >
+              <Plus />
+            </Button>
+            <div className='flex space-x-1.5 mx-auto items-center justify-center px-4'>
+              <Label htmlFor='name'>Q:</Label>
+              <p className='font-medium'>
+                {item.quantity} / {item.product.availableQuantity}
+              </p>
+            </div>
+            <Button
+              onClick={() => updateItemQuantity(item._id, item.quantity - 1)}
+              variant={item.quantity <= 1 ? 'ghost' : 'outline'}
+              disabled={item.quantity <= 1}
+              title={
+                item.quantity <= 1
+                  ? 'Click "Remove" instead'
+                  : 'Reduce quantity'
+              }
+            >
+              <Minus />
+            </Button>
+          </section>
+          <Button
+            variant='destructive'
+            onClick={() => removeItemFromCart(item._id)}
+          >
+            Remove
+          </Button>
+        </CardFooter>
+      </Card>
     </li>
   )
 }
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -3,11 +3,20 @@
 import { useQuery } from '@apollo/client'
 import { GET_PRODUCTS } from '@/graphql/queries'
 import { Product } from '@/types'
-import { useCart } from '@/context/global.context'
+import { useGlobalContext } from '@/context/global.context'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+import { Button } from './ui/button'
 
 const ProductList = () => {
   const { loading, error, data } = useQuery(GET_PRODUCTS)
-  const { addItemToCart } = useCart()
+  const { addItemToCart, cart } = useGlobalContext()
 
   if (loading) return <p>Loading products...</p>
   if (error) return <p>Error loading products.</p>
@@ -20,16 +29,30 @@ const ProductList = () => {
 
   return (
     <div>
-      <h2>Products</h2>
-      <ul>
+      <ul className='flex flex-col gap-5 mx-auto max-w-96'>
         {products.map((product: Product) => (
           <li key={product._id}>
-            <h3>{product.title}</h3>
-            <p>Cost: ${product.cost}</p>
-            <p>Available: {product.availableQuantity}</p>
-            <button onClick={() => handleAddToCart(product)}>
-              Add to Cart
-            </button>
+            <Card key={product._id}>
+              <CardHeader>
+                <CardTitle>{product.title}</CardTitle>
+                <CardDescription>Cost: ${product.cost}</CardDescription>
+              </CardHeader>
+
+              <CardContent>
+                <p>Available: {product.availableQuantity}</p>
+              </CardContent>
+
+              <CardFooter>
+                <Button
+                  onClick={() => handleAddToCart(product)}
+                  disabled={cart?.items.some(
+                    (item) => item.product._id === product._id
+                  )}
+                >
+                  Add to Cart
+                </Button>
+              </CardFooter>
+            </Card>
           </li>
         ))}
       </ul>
